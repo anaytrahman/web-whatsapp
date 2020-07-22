0b5fa4a1960bae4f709c22c92947a9670c676f62
Anayt Rahman <anaytrahman786@gmail.com>
2020-07-22 13:27:12 +0530

    added v2 version

---
+++ js/js.js
@@ -0,0 +1,7 @@
+
+					var inputSearch = document.querySelector('.input-search');
+					var searchBtn = document.querySelector('.search-btn');
+					searchBtn.addEventListener('click', function(){
+						inputSearch.style.display= "block";
+					});
+					
